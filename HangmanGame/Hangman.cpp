5b82312300365f313ab263274caa2c7090a4c1f2
Sogand <sogand4@students.cs.ubc.ca>
2022-08-26 18:26:04 -0700

    Implemented Hangman.cpp

--- HangmanGame/Hangman.cpp
+++ HangmanGame/Hangman.cpp
@@ -1,1 +1,85 @@
 #include "Hangman.h"
+#include <iostream>
+
+bool Hangman::hasBeenGuessed(char letter) {
+	return allGuesses.find(letter) != allGuesses.end();
+}
+
+bool Hangman::makeGuess(char letter) {
+	tries++;
+	return secretWord.find(letter) != std::string::npos;
+}
+
+void Hangman::displayUpdatedWord() {
+	for (int i = 0; i < secretWord.size(); i++) {
+		char currentLetter = secretWord.at(i);
+
+		if (allGuesses.find(currentLetter) != allGuesses.end()) {
+			std::cout << currentLetter;
+		}
+		else {
+			std::cout << "_";
+		}
+	}
+}
+
+void Hangman::displayMan() {
+	switch (tries) {
+	case 0:
+		std::cout << "    |    ";
+		break;
+	case 1:
+		std::cout << "    |    ";
+		std::cout << "    0    ";
+		break;
+	case 2:
+		std::cout << "    |    ";
+		std::cout << "    0    ";
+		std::cout << "    |    ";
+		break;
+	case 3:
+		std::cout << "    |    ";
+		std::cout << "    0    ";
+		std::cout << "    |    ";
+		std::cout << "   /     ";
+		break;
+	case 4:
+		std::cout << "    |    ";
+		std::cout << "    0    ";
+		std::cout << "    |    ";
+		std::cout << "   / \   ";
+		break;
+	case 5:
+		std::cout << "    |    ";
+		std::cout << "    0    ";
+		std::cout << "   /|    ";
+		std::cout << "   / \   ";
+		break;
+	case 6:
+		std::cout << "    |    ";
+		std::cout << "    0    ";
+		std::cout << "   /|\   ";
+		std::cout << "   / \   ";
+		break;
+	default:
+		break;
+	}
+}
+
+bool Hangman::gameWon() {
+	int numLettersGuessed = 0;
+
+	for (int i = 0; i < secretWord.size(); i++) {
+		char currentLetter = secretWord.at(i);
+
+		if (allGuesses.find(currentLetter) != allGuesses.end()) {
+			numLettersGuessed++;
+		}
+	}
+
+	return numLettersGuessed == secretWord.size();
+}
+
+bool Hangman::gameOver() {
+	return tries == TOTAL_NUM_TRIES;
+}
--- HangmanGame/Hangman.h
+++ HangmanGame/Hangman.h
@@ -1,14 +1,19 @@
+#include <string>
+#include <set>
+
 class Hangman {
 	const static int TOTAL_NUM_TRIES = 6;
 
-private:
+protected:
 	int tries;
-	char failedGuesses[TOTAL_NUM_TRIES];
-	char *secretWord;
+	int numLetters;
+	std::set<char> allGuesses;
+	std::string secretWord;
 
 public:
-	Hangman(char *word) {
+	Hangman(std::string word) {
 		tries = 0;
+		numLetters = word.length();
 		secretWord = word;
 	}
 
@@ -16,7 +21,7 @@ public:
 	bool hasBeenGuessed(char letter);
 
 	// REQUIRES: letter has not been guessed before
-	// EFFECTS: return true if letter is in the secret word, false otherwise
+	// EFFECTS: update numebr of tries. Return true if letter is in the secret word, false otherwise
 	bool makeGuess(char letter);
 
 	// EFFECTS: display all currently revealed letters to the player
@@ -31,6 +36,10 @@ public:
 	// EFFECTS: return true if player ran out of tries before they guessed all the letters
 	bool gameOver();
 
-	// EFFECTS: run through one round of hangman (ie. user makes one guess and recieves feedback on that guess)
-	void play(char letter);
+
+	// getters
+	int getTries() { return tries; }
+	int getNumLetters() { return numLetters; }
+	std::set<char> getallGuesses() { return allGuesses; }
+	std::string getSecretWord() { return secretWord; }
 };
